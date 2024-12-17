3f5bc633c2ba76da592f7b83a335c58faa0161e7
Nisarsaeed <dash75549@gmail.com>
2024-12-17 17:00:55 +0500

    feat: homepage hero section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,12 @@
 import { Highlight } from "./ui/highlight-text";
 import { MagicButton } from "./ui/magic-button";
-import Spacer from "./ui/spacer";
 
 function Hero() {
   return (
-    <div className="min-h-[70vh] container relative overflow-hidden flex justify-center items-center ">
-      <div className="text-primary z-20 w-1/2 flex flex-col justify-center items-center">
-        <h3 className="font-bold text-xl my-4  ">Discover Something Amazing</h3>
-        <h1 className="font-extrabold text-5xl my-4 leading-normal text-center">
+    <div className="min-h-screen container relative flex justify-center items-center">
+      <div className=" text-primary z-20 w-1/2 flex flex-col justify-center items-center relative">
+        <h3 className="font-bold text-xl mb-3">Discover Something Amazing</h3>
+        <h1 className="font-extrabold text-5xl mb-4 leading-normal text-center">
           Welcome to <br /> <Highlight>CheatSavant</Highlight>
         </h1>
         <p className="my-4 text-center w-3/4">
@@ -16,8 +15,15 @@ function Hero() {
           other games at the lowest prices.
         </p>
         <MagicButton title={"Explore Shop"} url={"/products"} />
-        <Spacer />
       </div>
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover opacity-60"
+        src="/bg-video.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
     </div>
   );
 }
