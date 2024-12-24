5f8fa275afef48bd8fba779054f613397ce9f99f
Nisarsaeed <dash75549@gmail.com>
2024-12-24 21:05:18 +0500

    card component imported from shadcn

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,42 +1,50 @@
-import { FaStar } from "react-icons/fa";
-import Image from "next/image";
-import Link from "next/link";
-import { TbPointFilled } from "react-icons/tb";
+import * as React from "react"
 
-function Card({ img, name, price, className,url }) {
-  return (
-    <div
-      className={`w-full max-w-sm border border-accentPurple rounded-lg shadow text-primary bg-accentBlue group p-4 ${className}`}
-    >
-      <Link href={url}>
-        <Image
-          height={50}
-          width={50}
-          className="object-cover w-full transform transition-transform duration-300 group-hover:scale-105 "
-          src={img}
-          alt="product image"
-        />
-        <div className="pt-4 flex justify-between">
-          <h2 className="text-lg col-span-2 font-semibold">{name}</h2>
-          <h3 className="text-lg opacity-70">£{price}</h3>
-        </div>
-        <div className="pt-4 flex justify-between">
-        <div className="flex items-center space-x-1 z-20">
-          {[...Array(5)].map((_, index) => (
-            <FaStar
-              key={index}
-              className="text-yellow-300 drop-shadow-[0_0_6px_rgba(255,223,0,0.7)]"
-            />
-          ))}
-        </div>
-        <div className="text-green-600 flex items-center space-x-1 font-semibold">
-          <TbPointFilled size={25} className="drop-shadow-[0_0_5px_rgba(0,255,0,0.8)]"/>
-          <span>Undetected</span>
-        </div>
-        </div>
-      </Link>
-    </div>
-  );
-}
+import { cn } from "@/lib/utils"
 
-export default Card;
+const Card = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("rounded-xl border bg-card text-card-foreground shadow", className)}
+    {...props} />
+))
+Card.displayName = "Card"
+
+const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    {...props} />
+))
+CardHeader.displayName = "CardHeader"
+
+const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("font-semibold leading-none tracking-tight", className)}
+    {...props} />
+))
+CardTitle.displayName = "CardTitle"
+
+const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props} />
+))
+CardDescription.displayName = "CardDescription"
+
+const CardContent = React.forwardRef(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+))
+CardContent.displayName = "CardContent"
+
+const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex items-center p-6 pt-0", className)}
+    {...props} />
+))
+CardFooter.displayName = "CardFooter"
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
