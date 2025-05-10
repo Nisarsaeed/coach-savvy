50de2d29464c5ef132aaf5ddf9703be36bfb6d15
Nisarsaeed <dash75549@gmail.com>
2025-05-10 20:20:49 +0500

    mongo uri added in env

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 require('dotenv').config();
 
-const uri = 'mongodb+srv://dash75549:AZu7gLlMr1bEVFf6@cluster0.hpyp8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri = process.env.MONGODB_URI;
 if (!uri) {
   throw new Error('Please define the MONGODB_URI environment variable in .env.local');
 }
