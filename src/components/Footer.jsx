f3a9f66e7c1b656666bb9efbaa0be35fc277cfc1
Nisarsaeed <dash75549@gmail.com>
2025-05-10 20:13:28 +0500

    ui revamped

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <div className='h-20 flex flex-col justify-center items-center container text-primary mt-24'>
-        <p>Copyright © 2024 - @Ace Owner</p>
-        <p className='my-4'>Developed by <a href="https://www.nisarportfolio.site/">Nisar</a></p>
+ return (
+  <footer className="text-primary py-6 mt-8">
+    <div className="container mx-auto flex items-center justify-between ">
+      <p className="text-sm">
+        © 2025 - <span className="font-semibold">CoachSavvy</span>
+      </p>
+      <p className="text-sm">
+        Developed by{" "}
+        <a
+          href="https://www.nisarportfolio.site/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 hover:underline"
+        >
+          Nisar
+        </a>
+      </p>
+      <div className="flex space-x-4 mt-2">
+        <a href="#" className="text-primary hover:text-blue-400">Facebook</a>
+        <a href="#" className="text-primary hover:text-blue-400">Twitter</a>
+        <a href="#" className="text-primary hover:text-blue-400">LinkedIn</a>
+      </div>
     </div>
-  )
+  </footer>
+);
+
 }
 
 export default Footer
