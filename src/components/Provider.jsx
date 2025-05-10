ad0c4bd163849ce584a12ac8f4bf670435551bac
Nisarsaeed <dash75549@gmail.com>
2025-05-10 20:18:11 +0500

    fix: import store as named function

--- src/components/Provider.jsx
+++ src/components/Provider.jsx
@@ -2,7 +2,7 @@
 'use client'; // Required to use hooks in a client component
 
 import { Provider } from 'react-redux';
-import store from '@/store';
+import { store } from '@/store';
 
 export default function Providers({ children }) {
   return <Provider store={store}>{children}</Provider>;
