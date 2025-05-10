647815243b398aa08108b5a74fe7ddbb1bec2fa3
Nisarsaeed <dash75549@gmail.com>
2025-05-10 20:07:25 +0500

    breadcrumbs removed from admin panel

--- src/app/ace/layout.js
+++ src/app/ace/layout.js
@@ -1,20 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { AppSidebar } from "@/components/Sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar";
-import ProductsTable from "@/components/ProductsTable";
+} from "@/components/ui/sidebar"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,19 +31,6 @@ export default function DashboardLayout({ children }) {
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    ACE Kingdom
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0 text-primary">
