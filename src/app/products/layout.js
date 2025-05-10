dbbb0687a918dc76fcf2082ded3a30e6ac22e784
Nisarsaeed <dash75549@gmail.com>
2025-05-10 20:10:46 +0500

    site title changed

--- src/app/products/layout.js
+++ src/app/products/layout.js
@@ -1,15 +1,15 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { GridBackground } from "@/components/ui/bg-grid";
 
 export const metadata = {
-  title: "CheatSavant | Products",
+  title: "CoachSavvy | Products",
   description: "Get all your valourant tools here",
 };
 
 export default function ProductPageLayout({ children }) {
   return (
     <div>
-        <Header/>
         {children}
         <Footer/>
     </div>
